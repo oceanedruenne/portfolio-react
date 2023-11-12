ddbd76de87e63ca8cc45bb7985c533dcae9c17a5
Océane Druenne <oceane.druenne2@etu.unistra.fr>
2023-11-12 12:58:17 +0100

    add target

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ export default function Header()
             <h1>Océane Druenne</h1>
             </div>
             <div className="links">
-                <h1><a href="https://github.com/oceanedruenne">GitHub</a></h1>
-                <h1><a href="mailto:druenneoceane@gmail.com">Email</a></h1>
-                <h1><a href="https://www.linkedin.com/in/oc%C3%A9ane-druenne-5933661ba/">Linkedin</a></h1>
+                <h1><a href="https://github.com/oceanedruenne" target="_blank">GitHub</a></h1>
+                <h1><a href="mailto:druenneoceane@gmail.com" target="_blank">Email</a></h1>
+                <h1><a href="https://www.linkedin.com/in/oc%C3%A9ane-druenne-5933661ba/" target="_blank">Linkedin</a></h1>
             </div>
         </div>
     )
